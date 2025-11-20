2a349486763f4b055fcad0e59ec645ae7a134b70
Yashan97 <s.d.y.anupama@gmail.com>
2025-11-20 23:31:04 +0530

    add new Sms Window

--- src/WaterTankGUI/StartMain.java
+++ src/WaterTankGUI/StartMain.java
@@ -7,6 +7,7 @@ public class StartMain {
         waterLevelObservable.addWaterLevelObserver(new Alarm());
         waterLevelObservable.addWaterLevelObserver(new Sms());
         waterLevelObservable.addWaterLevelObserver(new Display());
+        waterLevelObservable.addWaterLevelObserver(new Sms());
         new WaterTank(waterLevelObservable).setLocationRelativeTo(null);
         new WaterTank(waterLevelObservable).setVisible(true);
     }
